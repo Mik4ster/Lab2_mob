34a31b84ca0ee5586ea0f99e23163175c5d3315a
Mik4ster <vdovinm4xim@gmail.com>
2020-11-20 19:42:47 +0700

    Initial commit

---
+++ java_teach/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        boolean b = true;
+        Scanner num = new Scanner(System.in);
+        if (b|false){
+            System.out.println("Hi");
+        }
+    }
+}
